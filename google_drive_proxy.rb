02162e1a7e7fa22b696fe68001fb15216b958660
Tom Maher <tmaher@github.com>
2017-07-24 22:41:13 -0700

    working!

--- google_drive_proxy.rb
+++ google_drive_proxy.rb
@@ -17,19 +17,10 @@ class GoogleDriveProxy < Sinatra::Base
     'It is foo'
   end
 
-  get '/foo/:id' do
-    proxy = DriveProxy.new(params[:id])
-    "DATA\n#{proxy.data}\n"
-    #"over size limit: #{proxy.csrf?}\n"
-    #proxy.download_url
-    #resp = HTTParty.get("#{base_url}&id=#{params[:id]}")
-    #{}"foo has ID thinger #{params[:id]}, resp #{resp}\n"
-  end
-
   get '/gd/:id' do
     content_type 'application/octet-stream'
     stream do |out|
-      DriveProxy.new(params[:id], io: out).data
+      DriveProxy.new(params[:id], io: out).render_target
     end
   end
 end
--- lib/drive_proxy.rb
+++ lib/drive_proxy.rb
@@ -6,23 +6,30 @@ class DriveProxy
 
   base_uri "https://drive.google.com"
 
-  def small_download_url
+  def unconfirmed_url
     "/uc?export=download&id=#{@id}"
   end
 
   def initialize(object_id, io: nil)
     @id = object_id
     @io = io
-    @response = get small_download_url, follow_redirects: true
-    @token = csrf_token
+    @token = check_csrf_token
+    @target_url = chase_redirects
   end
 
-  def chase_redirects(url = small_download_url)
+  def chase_redirects(url = confirmed_url)
     while url
       resp = head(url, follow_redirects: false)
-      break if resp.code < 300
+      #puts "HEAD #{url}, code #{resp.code}"
+      puts "code: #{resp.code}, class #{resp.code.class}, headers #{resp.headers['location']}"
+      break if resp.code < 300 || resp.code >= 500
       return url if resp.code < 300
-      url = resp.code >= 300 && resp.code < 400 ? resp.headers.location : nil
+      url = if resp.code >= 300 && resp.code < 400
+        resp.headers['location']
+      else
+        nil
+      end
+      #url = resp.code >= 300 && resp.code < 400 ? resp.headers['location'] : nil
     end
     url
   end
@@ -31,31 +38,39 @@ class DriveProxy
     !@token.nil?
   end
 
-  def csrf_token
-    return nil if cookies.nil?
+  def check_csrf_token
+    head(unconfirmed_url, follow_redirects: false) # get cookies
     @token ||= begin
       tok = nil
-      cookies.each { |k,v| tok = v if k.start_with?('download_warning') }
+      unless cookies.nil?
+        cookies.each { |k,v| tok = v if k.start_with?('download_warning') }
+      end
       tok
     end
   end
 
-  def data
+  def confirmed_url
+    @confirmed_url ||= unconfirmed_url + (csrf? ? "&confirm=#{@token}" : "")
+  end
+
+  def render_target
+    get(@target_url, follow_redirects: false, stream_body: true) do |frag|
+      @io.write frag
+    end
+  end
+
+  def xxx
     if csrf?
-      large_url = "#{small_download_url}&confirm=#{csrf_token}"
-      resp = head(large_url, follow_redirects: false)
-      puts "code: #{resp.code}"
-      puts "headers: #{resp.headers}"
-      puts "large url #{large_url}"
-      codes = {}
+      large_url = "#{small_download_url}&confirm=#{@token}"
+      #resp = head(large_url, follow_redirects: false)
+      #puts "code: #{resp.code}"
+      #puts "headers: #{resp.headers}"
+      #puts "large url #{large_url}"
       get(large_url, follow_redirects: true, stream_body: true) do |frag|
         @io.write frag
       end
-      puts "codes: #{codes}"
     else
       @io << @response.body
     end
-
-    #"OVER LIMIT - NOT IMPLEMENTED - cookies #{cookies}"
   end
 end
